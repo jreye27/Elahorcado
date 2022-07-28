b7124698e0ae820f59b716e5909688101cfa340f
Juan Ignacio Reyes <jreyesmantilla@gmail.com>
2022-07-28 14:30:42 -0500

    Initial commit

---
+++ src/Actualizandovariables.java
@@ -0,0 +1,9 @@
+public class Actualizandovariables {
+    public static void main(String[] args) {
+        String name;
+        name = "Nacho";
+        name = name + " Reyes";
+        System.out.println("Tu nombre es: "+ name);
+    }
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Con esto estoy declarando una variable
+        int speed = 0;
+        // Con esto asignammos un valor diferente a la variable
+        speed = 10;
+        System.out.println(speed);
+        String employeename = "";
+        employeename = "Nacho";
+        System.out.println(employeename);
+
+
+
+
+    }
+}
